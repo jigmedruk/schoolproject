0144bf685a1ec61a181302819d27bbb370f520bd
unknown <user@Jigme1768.drukgreen.bt>
2020-09-10 01:30:52 +0600

    first

---
+++ advcbv/basic_app/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import (View, TemplateView,
+                                ListView, DetailView,
+                                CreateView, UpdateView,
+                                DeleteView)
+from . import models
+
+# Create your views here.
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self,**kwargs):
+        context =super().get_context_data(**kwargs)
+        context['injectme']= 'BASIC INJECTION'
+        return context
+
+class SchoolListView(ListView):
+    context_object_name ='schools'
+    model = models.School
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_details'
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
+
+class SchoolCreateView(CreateView):
+    fields = ('name','principal','location')
+    model = models.School
+
+class SchooUpdateView(UpdateView):
+    fields = ('name', 'principal')
+    model =models.School
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
